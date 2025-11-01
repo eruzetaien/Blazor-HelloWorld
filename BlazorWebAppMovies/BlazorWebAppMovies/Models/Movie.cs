d25265630df6be2d34ef023826c3b7e6f55da3ca
eruzetaien <muhammad.ruzain@ui.ac.id>
2025-11-02 06:42:50 +0700

    feat: add movie search

--- BlazorWebAppMovies/BlazorWebAppMovies/Models/Movie.cs
+++ BlazorWebAppMovies/BlazorWebAppMovies/Models/Movie.cs
@@ -21,5 +21,5 @@ public class Movie
     [Range(0, 100)]
     [DataType(DataType.Currency)]
     [Column(TypeName = "decimal(18, 2)")]
-    public decimal Price { get; set; }
+    public decimal Price { get; set; } = decimal.Zero;
 }
